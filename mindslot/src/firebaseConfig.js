6757675d6ed58b6a01ff96ecf670dbdf5d5ea1b2
Prithiv <theprithivrajs@gmail.com>
2025-06-11 18:16:15 +0530

    Update firebaseConfig.js

--- mindslot/src/firebaseConfig.js
+++ mindslot/src/firebaseConfig.js
@@ -3,16 +3,7 @@ import { getAuth, sendEmailVerification, createUserWithEmailAndPassword, signInW
 import { getDatabase, ref, set, get, update, remove } from "firebase/database";
 
 // ✅ Make sure databaseURL is correctly set
-const firebaseConfig = {
-  apiKey: "AIzaSyBGWFrXteigpLyIfiba73lYuX2AkwfrIu4",
-  authDomain: "mindslot-bb616.firebaseapp.com",
-  databaseURL: "https://mindslot-bb616-default-rtdb.firebaseio.com/", // ✅ Ensure this is set correctly
-  projectId: "mindslot-bb616",
-  storageBucket: "mindslot-bb616.appspot.com",
-  messagingSenderId: "952281983210",
-  appId: "1:952281983210:web:498bac3e73fc9884438f93",
-  measurementId: "G-NNPKP303ST",
-};
+
 
 // ✅ Initialize Firebase App
 const app = initializeApp(firebaseConfig);
